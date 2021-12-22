b10a6006ee4973cf7ea8a9b97414696edb86614b
Yacine Zahidi <yacine.zahidi@orange.com>
2021-12-22 11:14:55 +0100

    Updates

---
+++ precache-manifest.311e0a0109c6a9a0db541f66cc18ce3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d474a8cdba373e25152",
+    "url": "/testapp/static/css/main.cabe0147.chunk.css"
+  },
+  {
+    "revision": "3d474a8cdba373e25152",
+    "url": "/testapp/static/js/main.3d474a8c.chunk.js"
+  },
+  {
+    "revision": "62850c5f1a8543168279",
+    "url": "/testapp/static/js/1.62850c5f.chunk.js"
+  },
+  {
+    "revision": "30d3b82113d5091c3871",
+    "url": "/testapp/static/js/runtime~main.30d3b821.js"
+  },
+  {
+    "revision": "56682909354a462485006a0ddf65611a",
+    "url": "/testapp/index.html"
+  }
+];
