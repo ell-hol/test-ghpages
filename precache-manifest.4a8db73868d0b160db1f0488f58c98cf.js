726e840aaff46ba1324d283c424161304779af16
Yacine Zahidi <yacine.zahidi@orange.com>
2021-12-22 10:27:44 +0100

    Updates

---
+++ precache-manifest.4a8db73868d0b160db1f0488f58c98cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d474a8cdba373e25152",
+    "url": "/test-ghpages/static/css/main.cabe0147.chunk.css"
+  },
+  {
+    "revision": "3d474a8cdba373e25152",
+    "url": "/test-ghpages/static/js/main.3d474a8c.chunk.js"
+  },
+  {
+    "revision": "62850c5f1a8543168279",
+    "url": "/test-ghpages/static/js/1.62850c5f.chunk.js"
+  },
+  {
+    "revision": "6c4d9ba2767456b4a5db",
+    "url": "/test-ghpages/static/js/runtime~main.6c4d9ba2.js"
+  },
+  {
+    "revision": "4fe0d1e8aec25e6d1ffb89d272491bbf",
+    "url": "/test-ghpages/index.html"
+  }
+];
